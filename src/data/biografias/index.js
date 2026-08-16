0661d5e8cfd022ddde40a57823ff2f3f91273316
Gonzalo Escarpa <escarpaimacp@gmail.com>
2026-08-16 17:41:05 +0200

    Crear índice de metas de biografias

---
+++ src/data/biografias/index.js
@@ -0,0 +1,30 @@
+// Índice de metas de biografias detalladas
+import { DICKINSON_META } from './201-dickinson.js';
+import { NERUDA_META } from './202-neruda-meta.js';
+import { LORCA_META } from './203-lorca-meta.js';
+import { SORJUANA_META } from './204-sorjuana-meta.js';
+import { VALLEJO_META } from './205-vallejo-meta.js';
+import { PLATH_META } from './206-plath-meta.js';
+import { LORCA_FUSILADO_META } from './340-lorca-fusilado-meta.js';
+import { ALBERTI_EXILIO_META } from './341-alberti-exilio-meta.js';
+import { NERUDA_POLITICO_META } from './342-neruda-politico-meta.js';
+import { GONGORA_QUEVEDO_META } from './343-gongora-quevedo-meta.js';
+import { SORJUANA_RENUNCIA_META } from './344-sorjuana-renuncia-meta.js';
+import { BECQUER_PERIODISTA_META } from './345-becquer-periodista-meta.js';
+
+export const BIOGRAFIAS_METAS = {
+  [DICKINSON_META.id]: DICKINSON_META,
+  [NERUDA_META.id]: NERUDA_META,
+  [LORCA_META.id]: LORCA_META,
+  [SORJUANA_META.id]: SORJUANA_META,
+  [VALLEJO_META.id]: VALLEJO_META,
+  [PLATH_META.id]: PLATH_META,
+  [LORCA_FUSILADO_META.id]: LORCA_FUSILADO_META,
+  [ALBERTI_EXILIO_META.id]: ALBERTI_EXILIO_META,
+  [NERUDA_POLITICO_META.id]: NERUDA_POLITICO_META,
+  [GONGORA_QUEVEDO_META.id]: GONGORA_QUEVEDO_META,
+  [SORJUANA_RENUNCIA_META.id]: SORJUANA_RENUNCIA_META,
+  [BECQUER_PERIODISTA_META.id]: BECQUER_PERIODISTA_META,
+};
+
+export default BIOGRAFIAS_METAS;
